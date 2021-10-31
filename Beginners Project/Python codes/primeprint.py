b8e829b3ebf1d6ea084016b78ee79719aa62fe28
Soham Maitra <49953213+nitroidzero@users.noreply.github.com>
2021-10-31 19:04:38 +0530

    Create primeprint.py

---
+++ Beginners Project/Python codes/primeprint.py
@@ -0,0 +1,9 @@
+end = int(inout())
+ 
+for i in range(1, end+1):
+    if i > 1:
+        for j in range(2, i):
+            if(i % j == 0):
+                break
+        else:
+            print(i)
