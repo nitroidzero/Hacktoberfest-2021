2d24441b7d2f431229f8679046ae7245f641457f
Soham Maitra <49953213+nitroidzero@users.noreply.github.com>
2021-10-31 19:08:19 +0530

    Delete primeprint.py

--- Beginners Project/Python codes/primeprint.py
+++
@@ -1,9 +0,0 @@
-end = int(input())
- 
-for i in range(1, end+1):
-    if i > 1:
-        for j in range(2, i):
-            if(i % j == 0):
-                break
-        else:
-            print(i)
